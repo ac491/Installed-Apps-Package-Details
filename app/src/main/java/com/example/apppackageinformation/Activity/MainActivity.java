c94141ed97b79b01ea9d2ecdeee12364d68c1cb8
arko chatterjee <ac491@snu.edu.in>
2019-06-29 00:47:04 +0530

    final app

--- app/src/main/java/com/example/apppackageinformation/Activity/MainActivity.java
+++ app/src/main/java/com/example/apppackageinformation/Activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.apppackageinformation.Activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,6 +19,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.Html;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,6 +32,11 @@ import com.example.apppackageinformation.Model.AppDetails;
 import com.example.apppackageinformation.R;
 
 import org.xml.sax.XMLReader;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private View mLine1;
     private View mLine2;
     private View mLine3;
+    private ImageView mAlertImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +71,10 @@ public class MainActivity extends AppCompatActivity {
         mLine1 = findViewById(R.id.lineView1);
         mLine2 = findViewById(R.id.lineView2);
         mLine3 = findViewById(R.id.lineView3);
+        mAlertImageView = findViewById(R.id.noAppsIv);
+        mAlertImageView.setVisibility(View.GONE);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -81,14 +94,106 @@ public class MainActivity extends AppCompatActivity {
             if(byteArray != null) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
                 appIcon = new BitmapDrawable(getResources(), bitmap);
-            } else {
-                mIconImageView.setVisibility(View.GONE);
             }
         }
+
+        //fetch details of previously opened app
+        if(pref.getString("appName", null) != null) {
+
+            String appName = pref.getString("appName", null);
+            String packageName = pref.getString("packageName", null);
+            Set<String> permissions = pref.getStringSet("permissions", null);
+            Set<String> recievers = pref.getStringSet("recievers", null);
+            Set<String> services = pref.getStringSet("services", null);
+
+            try {
+                if (packageName != null) {
+                    Drawable icon = getPackageManager().getApplicationIcon(packageName);
+                    mIconImageView.setImageDrawable(icon);
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            mAppNameTextView.setText(appName);
+
+            if (permissions != null) {
+                String message = "<html><body>";
+                for (String permission : permissions) {
+                    message += "- " + permission + "<br/><br/>";
+                }
+                message += "</body></html>";
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    mPermissionsList.setText(Html.fromHtml(message, Html.FROM_HTML_MODE_COMPACT));
+                } else {
+                    mPermissionsList.setText(Html.fromHtml(message));
+                }
+
+            } else {
+                mPermissionTextView.setVisibility(View.GONE);
+                mPermissionsList.setVisibility(View.GONE);
+                mLine1.setVisibility(View.GONE);
+            }
+
+            if (recievers != null) {
+                String message = "<html><body>";
+                for (String reciever : recievers) {
+                    message += "- " + reciever + "<br/><br/>";
+                }
+                message += "</body></html>";
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    mRecieversList.setText(Html.fromHtml(message, Html.FROM_HTML_MODE_COMPACT));
+                } else {
+                    mRecieversList.setText(Html.fromHtml(message));
+                }
+
+            } else {
+                mRecieversTextView.setVisibility(View.GONE);
+                mRecieversList.setVisibility(View.GONE);
+                mLine2.setVisibility(View.GONE);
+            }
+
+            if (services != null) {
+
+                String message = "<html><body>";
+                for (String service : services) {
+                    message += "- " + service + "<br/><br/>";
+                }
+                message += "</body></html>";
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    mServicesList.setText(Html.fromHtml(message, Html.FROM_HTML_MODE_COMPACT));
+                } else {
+                    mServicesList.setText(Html.fromHtml(message));
+                }
+
+            } else {
+                mServicesTextView.setVisibility(View.GONE);
+                mServicesList.setVisibility(View.GONE);
+                mLine3.setVisibility(View.GONE);
+            }
+
+        }
+
+        //display details of the app which is clicked.
         if (appDetails != null) {
+
+            SharedPreferences.Editor editor = pref.edit();
             Log.d("TAG", "name" + appDetails.getAppName());
-            mIconImageView.setImageDrawable(appIcon);
+            if (appDetails.getPackageName() != null) {
+                Drawable icon = null;
+                try {
+                    icon = getPackageManager().getApplicationIcon(appDetails.getPackageName());
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+                mIconImageView.setImageDrawable(icon);
+            }
             mAppNameTextView.setText(appDetails.getAppName());
+
+            editor.putString("appName", appDetails.getAppName());
+            editor.putString("packageName", appDetails.getPackageName());
 
             if (appDetails.getAppPermissions() != null) {
                 //ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, appDetails.getAppPermissions());
@@ -103,6 +208,12 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     mPermissionsList.setText(Html.fromHtml(message));
                 }
+
+                List<String> permissions = Arrays.asList(appDetails.getAppPermissions());
+                Set<String> permissionSet = new HashSet<String>();
+                permissionSet.addAll(permissions);
+                editor.putStringSet("permissions", permissionSet);
+
             } else {
                 mPermissionTextView.setVisibility(View.GONE);
                 mPermissionsList.setVisibility(View.GONE);
@@ -128,6 +239,12 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     mRecieversList.setText(Html.fromHtml(message));
                 }
+
+                List<String> recieversList = Arrays.asList(recievers);
+                Set<String> recieverSet = new HashSet<String>();
+                recieverSet.addAll(recieversList);
+                editor.putStringSet("recievers", recieverSet);
+
             } else {
                 mRecieversTextView.setVisibility(View.GONE);
                 mRecieversList.setVisibility(View.GONE);
@@ -153,12 +270,20 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     mServicesList.setText(Html.fromHtml(message));
                 }
+
+                List<String> servicesList = Arrays.asList(services);
+                Set<String> servicesSet = new HashSet<String>();
+                servicesSet.addAll(servicesList);
+                editor.putStringSet("services", servicesSet);
+
             } else {
                 mServicesTextView.setVisibility(View.GONE);
                 mServicesList.setVisibility(View.GONE);
                 mLine3.setVisibility(View.GONE);
             }
-        } else {
+
+            editor.commit();
+        } else if (pref.getString("packageName", null) == null) {
             mRecieversList.setVisibility(View.GONE);
             mRecieversTextView.setVisibility(View.GONE);
             mServicesTextView.setVisibility(View.GONE);
@@ -168,6 +293,9 @@ public class MainActivity extends AppCompatActivity {
             mLine1.setVisibility(View.GONE);
             mLine2.setVisibility(View.GONE);
             mLine3.setVisibility(View.GONE);
+            mAppNameTextView.setGravity(Gravity.CENTER_VERTICAL);
+            mIconImageView.setVisibility(View.GONE);
+            mAlertImageView.setVisibility(View.VISIBLE);
         }
 
     }
